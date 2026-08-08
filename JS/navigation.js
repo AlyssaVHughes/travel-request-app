4899483a5bb4c4ae1fabf75d1033783cdfb068bc
Alyssa Hughes <alyssavhughes@outlook.com>
2026-08-07 22:25:26 -0400

    created the html and styling for the request table

--- JS/navigation.js
+++ JS/navigation.js
@@ -16,11 +16,13 @@ const sidebar = document.querySelector(".sidebar");
 const sidebarHeader = document.querySelector(".sidebar-header");
 const sidebarButton = document.querySelector(".sidebar-button");
 const toggleBtn = document.querySelector(".sidebar-logo");
+const approvalNotif = document.querySelector(".approval-notif");
 
 toggleBtn.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
     sidebarHeader.classList.toggle("collapsed");
     sidebarButton.classList.toggle("collapsed");
+    approvalNotif.classList.toggle("collapsed");
 
     if (sidebar.classList.contains("collapsed")) {
         toggleBtn.src = "../Images/Icons/open_icon.png";
